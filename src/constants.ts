417435fbae8ae07734070c6bf8e881684e8e0c64
derricksimpson <derrick.simpson@gmail.com>
2024-12-27 12:03:25 -0500

    chore: move files to root

---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+export const DEFAULT_FILES = [
+  {
+    id: "html",
+    name: "index.html",
+    content:
+      "<!DOCTYPE html>\n<html>\n  <body>\n    <h1>Hello World</h1>\n  </body>\n</html>",
+    language: "html",
+  },
+  {
+    id: "css",
+    name: "styles.css",
+    content: "h1 { color: blue; }",
+    language: "css",
+  },
+  {
+    id: "js",
+    name: "script.js",
+    content: 'console.log("Hello from JavaScript!");',
+    language: "javascript",
+  },
+];
